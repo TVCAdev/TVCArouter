06cb7980d484c6a0784ec37e0ae07d601376ecaa
TVCAdev <TVCAdev@gmail.com>
2021-12-18 22:39:10 +0900

    replace request module with axios module.

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:latest
+
+ENV TOP_DIR /TVCArouter
+  
+RUN mkdir ${TOP_DIR}
+
+COPY tvca_router.js ${TOP_DIR}
+COPY package.json ${TOP_DIR}
+COPY package-lock.json ${TOP_DIR}
+
+WORKDIR ${TOP_DIR}
+RUN npm install
+
+CMD ["/usr/local/bin/node","tvca_router.js"]
+
--- tvca_router.js
+++ tvca_router.js
@@ -2,11 +2,11 @@
 
 const fs = require("fs")
 
-const request = require("request");
+const axios = require("axios");
 const io_client = require("socket.io-client");
 
 // connect to heroku using websocket
-let socket_client = io_client(process.env.LINEBOT_URL, {
+const socket_client = io_client(process.env.LINEBOT_URL, {
     query: {
         token: process.env.WEBSOCKET_TOKEN,
     },
@@ -14,26 +14,44 @@ let socket_client = io_client(process.env.LINEBOT_URL, {
 
 // websocket message recieve
 socket_client.on("GET_LIVINGPIC", () => {
-    console.log("GET_LIVINGPIC");
+    console.log("GET_LIVINGPIC was accept...");
 
-    let options = {
-        url: process.env.WEBAPI_POST_MSG_URL,
-        json: true,
-    };
+    axios({
+        method: 'get',
+        url: process.env.WEBAPI_POST_MSG_URL + "GET_LIVINGPIC",
+    })
+        .then(function (response) {
+            // case of getting camera data successfully
+            if (typeof response !== "undefined" && response.status == 200) {
+                socket_client.emit("GET_LIVINGPIC", response.data);
 
-    request.get(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            socket_client.conn.emit("GET_LIVINGPIC", body);
-
-            // for debug code
-            //let decode_file = Buffer.from(body.data, 'base64');
-            //fs.writeFileSync("aaa.jpg", decode_file);
-        } else if (typeof response === "undefined") {
-            console.log('error: response is undefined.');
-        } else if (typeof response.statusCode === "undefined") {
-            console.log('error: response.statusCode is undefined.');
-        } else {
-            console.log('error: ' + response.statusCode);
-        }
-    });
+                // for debug code
+                //let decode_file = Buffer.from(response.data.imgdata, 'base64');
+                //fs.writeFileSync("aaa.jpg", decode_file);
+            } else if (typeof response === "undefined") {
+                console.log('GET_LIVINGPIC: error - response is undefined.');
+            } else if (typeof response.status === "undefined") {
+                console.log('GET_LIVINGPIC: error - response.status is undefined.');
+            } else {
+                console.log('GET_LIVINGPIC: error - status is ' + response.status);
+            }
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+            console.log(error.config);
+        });
 });
